ad9a53be61da11aef1170bd3d13951d76f86c143
Phil Peng <phil.peng@sap.com>
2015-07-22 13:22:35 +0800

    WIP

--- js/main.js
+++ js/main.js
@@ -1,58 +1,69 @@
-$(function () {
-	var $siteUrl = $('.url-site'),
-		$main = $('.main'),
-		$sourceUrl = $('.url-source'),
-		$replaceUrl = $('.url-replace'),
-		$type = $('.type-select'),
-		$save = $('.btn-save'),
-		$clean = $('.btn-clean');
+$(function() {
+  var $siteUrl = $('.url-site'),
+    $main = $('.main'),
+    $sourceUrl = $('.url-source'),
+    $replaceUrl = $('.url-replace'),
+    $type = $('.type-select'),
+    $save = $('.btn-save'),
+    $clean = $('.btn-clean'),
+    $input = $('.data-input');
 
-	var PROP_SITE_URL = 'siteUrl',
-		PROP_SOURCE_URL = 'sourceUrl',
-		PROP_REPLACE_URL = 'replaceUrl';
+  var PROP_SITE_URL = 'siteUrl',
+    PROP_SOURCE_URL = 'sourceUrl',
+    PROP_REPLACE_URL = 'replaceUrl';
 
-	var st = window.localStorage;
+  var st = window.localStorage;
 
-	function ani(type) {
-		$main.addClass(type ? 'done' : 'error');
-		setTimeout(function () {
-			$main.removeClass(type ? 'done' : 'error');
-		}, 1000);
-	}
+  function ani(type) {
+    $main.addClass(type ? 'done' : 'error');
+    setTimeout(function() {
+      $main.removeClass(type ? 'done' : 'error');
+    }, 1000);
+  }
 
-	chrome.storage.local.get(function (result) {
-		if (Object.getOwnPropertyNames(result).length > 0) {
-			$siteUrl.val(result[PROP_SITE_URL]);
-			$sourceUrl.val(result[PROP_SOURCE_URL]);
-			$replaceUrl.val(result[PROP_REPLACE_URL]);
-		}
-	});
+  chrome.storage.local.get(function(result) {
+    if (Object.getOwnPropertyNames(result).length > 0) {
+      $siteUrl.val(result[PROP_SITE_URL]);
+      $sourceUrl.val(result[PROP_SOURCE_URL]);
+      $replaceUrl.val(result[PROP_REPLACE_URL]);
+    }
+  });
 
-	$save.click(function () {
-		var siteVal = $siteUrl.val(),
-			sourceVal = $sourceUrl.val(),
-			replaceVal = $replaceUrl.val(),
-			typeVal = $type.val();
-		if (siteVal.length > 0 && sourceVal.length > 0 && replaceVal.length > 0 && typeVal.length > 0) {
-			chrome.storage.local.set({
-				'siteUrl': siteVal,
-				'sourceUrl': sourceVal,
-				'replaceUrl': replaceVal,
-				'type': typeVal
-			}, function () {
-				ani(1);
-			});
-		} else {
-			ani(0);
-		}
-	});
+  function save() {
+    var siteVal = $siteUrl.val(),
+      sourceVal = $sourceUrl.val(),
+      replaceVal = $replaceUrl.val(),
+      typeVal = $type.val();
+    if (siteVal.length === 0 && sourceVal.length === 0 && replaceVal.length === 0) {
+      ani(0);
+    } else {
+      var obj = {
+        type: typeVal
+      };
+      siteVal.length > 0 && (obj[PROP_SITE_URL] = siteVal);
+      sourceVal.length > 0 && (obj[PROP_SOURCE_URL] = sourceVal);
+      replaceVal.length > 0 && (obj[PROP_REPLACE_URL] = replaceVal);
+      chrome.storage.local.set(obj, function() {
+        ani(1);
+      });
+    }
+  };
 
-	$clean.click(function () {
-		chrome.storage.local.clear(function () {
-			$siteUrl.val('');
-			$sourceUrl.val('');
-			$replaceUrl.val('');
-			ani(1);
-		});
-	});
+  $clean.on('click', function() {
+    chrome.storage.local.clear(function() {
+      $siteUrl.val('');
+      $sourceUrl.val('');
+      $replaceUrl.val('');
+      ani(1);
+    });
+  });
+
+  $input.on('keypress', function(e) {
+    e.keyCode === 13 && $(e.currentTarget).blur();
+  });
+
+  $input.on('blur', function() {
+    save();
+  });
+
 });
